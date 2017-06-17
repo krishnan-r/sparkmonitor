04c5531829e7a33f9d1c179e234e55325d27a00e
krishnan-r <krishnanr1997@gmail.com>
2017-06-17 21:26:56 +0530

    Added Scala based listeners Basic Communication using sockets between python and scala py4j is completely bypassed Display not working yet

--- extension/scala/jupyterspark/src/main/scala/sparkmonitor/listener/CustomListener.scala
+++ extension/scala/jupyterspark/src/main/scala/sparkmonitor/listener/CustomListener.scala
@@ -6,87 +6,84 @@ import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
 import org.apache.spark._
 
-
 import org.apache.spark.SparkContext
 
+import java.net._
+import java.io._
 
 class PythonNotifyListener(conf: SparkConf) extends SparkListener {
-  println("Configuration: "+conf)
+	println("SPARKLISTENER: Started ScalaListener Constructor")
+  	println("SPARKLISTENER: Configuration: "+conf+"\n")
+	
+	val port = conf.get("sparkmonitor.port")
+  	println("SPARKLISTENER: connecting to port"+conf.get("sparkmonitor.port"))
 
-  override def onJobStart(jobStart: SparkListenerJobStart): Unit = synchronized {
-    println("SPARKLISTENER: Job Started" + jobStart)
+	val socket = new Socket("localhost",port.toInt)
+	val out = new OutputStreamWriter(socket.getOutputStream())
 
- val jobGroup = for (
-      props <- Option(jobStart.properties);
-      group <- Option(props.getProperty("spark.jobGroup.id"))
-    ) yield group
+	def send(msg:String):Unit={
+		println("\nSPARKLISTENER: --------------Sending Message:------------------\n"+msg+
+		"\nSPARKLISTENER: -------------------------------------------------\n")
+		out.write(msg+";EOD:")
+		out.flush()
+	}
+
+	def closeConnection():Unit={
+		println("SPARKLISTNER: Closing Connection")
+		out.close()
+		socket.close()
+	}
+
+//--------------Listener Overrided functions-------------------------------------------------
+
+	
+	override def onApplicationStart(appStarted: SparkListenerApplicationStart):Unit = {
+		println("SPARKLISTENER Application Started...Start Time: "+appStarted.time)
+		send("SPARKLISTENER Application Started...Start Time: "+appStarted.time)
+  	}
+
+ 	override def onApplicationEnd(appEnded: SparkListenerApplicationEnd):Unit =  {
+		println("SPARKLISTENER Application ending...End Time: "+appEnded.time)
+		send("SPARKLISTENER Application ending...End Time: "+appEnded.time)
+  	}
+
+	override def onJobStart(jobStart: SparkListenerJobStart): Unit = synchronized {
+
+		val jobGroup = for (
+	    	props <- Option(jobStart.properties);
+			group <- Option(props.getProperty("spark.jobGroup.id"))
+			) yield group
+
+ 		val numTasks= {
+			val allStages = jobStart.stageInfos
+			val missingStages = allStages.filter(_.completionTime.isEmpty)
+			missingStages.map(_.numTasks).sum
+		}
+
+		val json= ("jobGroup" -> jobGroup) ~
+			("jobId" -> jobStart.jobId) ~
+			("status" -> "RUNNING") ~
+			("submissionTime" -> Option(jobStart.time).filter(_ >= 0)) ~
+			("stageIds" -> jobStart.stageIds) ~
+			("numTasks" -> numTasks)
+		 println("SPARKLISTENER JobStart: \n"+ pretty(render(json)) + "\n")
+
+	}
+
+	override def onJobEnd(jobEnd: SparkListenerJobEnd): Unit = synchronized {
+
+		val json=("jobId" -> jobEnd.jobId) ~
+    	("completionTime" -> Option(jobEnd.time).filter(_ >= 0))
+
+ 		println("SPARKLISTENER Job End: \n"+ pretty(render(json)) + "\n")
  
- val numTasks= {
-      val allStages = jobStart.stageInfos
-      val missingStages = allStages.filter(_.completionTime.isEmpty)
-      missingStages.map(_.numTasks).sum
-    }
+  	}
 
-val json= ("jobGroup" -> jobGroup) ~
-          ("jobId" -> jobStart.jobId) ~
-          ("status" -> "RUNNING") ~
-          ("submissionTime" -> Option(jobStart.time).filter(_ >= 0)) ~
-          ("stageIds" -> jobStart.stageIds) ~
-          ("numTasks" -> numTasks)
-
- println("SPARKLISTENER JobStart: "+ pretty(render(json)))
-
-  }
-  
-
-override def onJobEnd(jobEnd: SparkListenerJobEnd): Unit = synchronized {
-   
-    var status
-    jobEnd.jobResult match {
-      case JobSucceeded =>
-        status = "SUCCEEDED"
-      case JobFailed(_) =>
-        status = "FAILED"
-      }
-     
-   val json=("jobId" -> jobEnd.jobId) ~
-          ("status" -> status) ~
-          ("completionTime" -> Option(jobEnd.time).filter(_ >= 0)) ~
-          ("stageIds" -> jobStart.stageIds) ~
-          ("numTasks" -> numTasks)
-
- println("SPARKLISTENER JobStart: "+ pretty(render(json)))
- 
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  override def onTaskEnd(taskEnd: SparkListenerTaskEnd) {
-    val recordsWritten = taskEnd.taskMetrics.outputMetrics.recordsWritten
-    //val message = compact(render(
-//      ("recordsWritten" -> recordsWritten)
-  //  ))
-    //Manager.notifyAll(message)
-  }
 }
 
+
 object PythonNotifyListener {
-  def main(args: Array[String]): Unit = {
-    println("SPARKLISTENER: Main Started (not called from spark)")
-  }
+	def main(args: Array[String]): Unit = {
+		println("SPARKLISTENER: Main Started (not called from spark)")
+	}
 }
