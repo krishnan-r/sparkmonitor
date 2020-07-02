e1af231c4ab969a9b1c5afb2dce8a979981e1ddb
Ben Epstein <ben.epstein97@gmail.com>
2020-07-02 17:45:34 -0400

    node modules

--- extension/js/node_modules/@types/node/buffer.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "buffer" {
-    export const INSPECT_MAX_BYTES: number;
-    const BuffType: typeof Buffer;
-    const SlowBuffType: typeof SlowBuffer;
-    export { BuffType as Buffer, SlowBuffType as SlowBuffer };
-}
--- extension/js/node_modules/mapbox-gl/js/util/browser/web_worker.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var WebWorkify = require('webworkify');
-
-module.exports = function () {
-    return new WebWorkify(require('../../source/worker'));
-};
